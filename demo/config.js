65b512d8a337b5b98a29b3a394a4ff481ad9d638
Rusty Tanton <rusty@perfectsensedigital.com>
2015-07-17 11:02:12 -0400

    added demo

---
+++ demo/config.js
@@ -0,0 +1,10 @@
+System.config({
+  defaultJSExtensions: true,
+  map: {
+  	'babel': '/assets/node/babel-core/browser.js',
+  	'bsp-utils': '/assets/bower/bsp-utils/bsp-utils.js',
+  	'bsp-useragent': '/assets/js/bsp-useragent.js',
+  	'jquery': '/assets/bower/jquery/dist/jquery.js'
+  },
+  transpiler: 'babel'
+});
